0dc38a764d90eda7a5a49e7fb001aa3d6598bb57
error <error@error-unlimited.net>
2018-07-16 18:12:51 -0500

    don't depend on rxjs internals

--- src/zone-scheduler.ts
+++ src/zone-scheduler.ts
@@ -1,36 +1,89 @@
 import { NgZone } from '@angular/core';
+import { SchedulerLike, SchedulerAction, Subscription } from 'rxjs';
 
-import { AsyncAction } from 'rxjs/internal/scheduler/AsyncAction';
-import { AsyncScheduler } from 'rxjs/internal/scheduler/AsyncScheduler';
+export class ZoneAction<T> extends Subscription {
+	public id: any;
+	public state: T;
+	public delay: number;
+	private pending: boolean = false;
 
-export class ZoneScheduler extends AsyncScheduler {
-	public constructor( public readonly ngZone: NgZone ) {
-		super( AsyncAction );
+	constructor( private scheduler: ZoneScheduler, private work: ( this: SchedulerAction<T>, state?: T ) => void ) {
+		super();
 	}
 
-	public flush( action: AsyncAction<any> ) {
-		const { actions, ngZone } = this;
+	public schedule( state?: T, delay: number = 0 ): Subscription {
+		if( this.closed ) return this;
+		this.state = state;
+		const id = this.id;
+		const scheduler = this.scheduler;
+		if( id != null ) this.id = this.recycleAsyncId( scheduler, id, delay );
+		this.pending = true;
+		this.delay = delay;
+		this.id = this.id || this.requestAsyncId( scheduler, this.id, delay );
+		return this;
+	}
 
-		if( this.active ) {
-			actions.push( action );
-			return;
-		}
+	private requestAsyncId( scheduler: ZoneScheduler, id?: any, delay: number = 0 ): any {
+		return setInterval( scheduler.flush.bind( scheduler, this ), delay );
+	}
 
-		this.active = true;
+	private recycleAsyncId( scheduler: ZoneScheduler, id: any, delay: number = 0 ): any {
+		if( delay !== null && this.delay === delay && this.pending === false ) return id;
+		return clearInterval( id ) && undefined || undefined;
+	}
 
-		let error: any;
-		do {
-			if( error = ngZone.run( () =>
-				action.execute( action.state, action.delay )
-			) ) {
-				break;
-			}
-		} while( action = actions.shift() );
-
-		this.active = false;
-
+	public execute( state: T, delay: number ): any {
+		if( this.closed ) return new Error( 'executing a cancelled action' );
+		this.pending = false;
+		const error = this._execute(state, delay);
 		if( error ) {
-			while( action = actions.shift() ) {
+			return error;
+		} else if( this.pending === false && this.id != null ) {
+			this.id = this.recycleAsyncId( this.scheduler, this.id, null );
+		}
+	}
+
+	private _execute( state: T, delay: number ): any {
+		let errored: boolean = false;
+		let errorValue: any = undefined;
+		try {
+			this.work( state );
+		} catch( e ) {
+			errored = true;
+			errorValue = !!e && e || new Error( e );
+		}
+		if( errored ) {
+			this.unsubscribe();
+			return errorValue;
+		}
+	}
+}
+
+export class ZoneScheduler implements SchedulerLike {
+	public constructor( public readonly ngZone: NgZone ) {}
+
+	public now = Date.now ? Date.now : () => +new Date();
+	public actions: Array<ZoneAction<any>> = [];
+
+	public schedule<T>( work: (this: SchedulerAction<T>, state?: T) => void, delay: number = 0, state?: T ): Subscription {
+		return new ZoneAction<T>( this, work ).schedule( state, delay );
+	}
+
+	public flush( action?: ZoneAction<any> ): void {
+		const { actions, ngZone } = this;
+		let error: any;
+		let index = -1;
+		let count = actions.length;
+		action = action || actions.shift();
+		do {
+			try {
+				error = ngZone.run( () => action.execute( action.state, action.delay ) );
+			} catch( e ) {
+				error = e;
+			}
+		} while( ++index < count && ( action = actions.shift() ) );
+		if( error ) {
+			while( ++index < count && ( action = actions.shift() ) ) {
 				action.unsubscribe();
 			}
 			throw error;
